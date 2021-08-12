8e16e9dfe2aaa8dc5a5296cb2f93e5e05d17992b
PEI-YUN LEE <may1092200258@gmail.com>
2021-08-13 00:44:34 +0800

    [add] - load css and js

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -1,10 +1,22 @@
 <?php
 
-function portfolio_register_styles(){
-    wp_enqueue_style('portfolio_register_styles',get_template_directory_uri(),"/style.css",array().'1.0','all');
+function portfolio_register_styles()
+{
 
+    $version = wp_get_theme()->get('Version');
+
+    wp_enqueue_style('portfolio-style', get_template_directory_uri() . '/style.css', array('portfolio-normalize'), $version, 'all');
+    wp_enqueue_style('portfolio-normalize', get_template_directory_uri() . '/assets/css/normalize.css', array(), '8.0.1', 'all');
 }
 
-add_action('wp_wnqueue_scripts','portfolio_register_styles')
+add_action('wp_enqueue_scripts', 'portfolio_register_styles');
 
-?>
+function portfolio_register_scripts()
+{
+
+    $version = wp_get_theme()->get('Version');
+
+    wp_enqueue_script('portfolio-js', get_template_directory_uri() . '/assets/js/index.js', array(), $version, true);
+}
+
+add_action('wp_enqueue_scripts', 'portfolio_register_scripts');
